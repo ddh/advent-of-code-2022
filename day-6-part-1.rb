24b7d20ef7f38d1b35ce7b0440b5b6f5e556fc88
Arthur Huynh <9056483+ddh@users.noreply.github.com>
2022-12-05 22:12:44 -0800

    Day 6 - cut down on lines

--- day-6-part-1.rb
+++ day-6-part-1.rb
@@ -3,20 +3,18 @@
 # checking if consecutive 4-letters is unique
 
 def read(file)
-  counter = 0
   uniq_chars = []
   File.readlines(file).each do |line|
-    line.chars.each do |char|
-      break if uniq_chars.uniq.count == 4
+    line.chars.each_with_index do |char, index|
+      if uniq_chars.uniq.count == 4
+        print index # Prints answer
+        break
+      end
 
       uniq_chars << char
-      counter += 1
-      uniq_chars = uniq_chars.drop(1) if uniq_chars.length > 4
+      uniq_chars.slice!(0) if uniq_chars.length > 4
     end
   end
-
-  # Print answer:
-  print counter
 end
 
 read('day-6-input.txt')
--- day-6-part-2.rb
+++ day-6-part-2.rb
@@ -3,20 +3,18 @@
 # checking if consecutive 14-letters is unique
 
 def read(file)
-  counter = 0
   uniq_chars = []
   File.readlines(file).each do |line|
-    line.chars.each do |char|
-      break if uniq_chars.uniq.count == 14
+    line.chars.each_with_index do |char, index|
+      if uniq_chars.uniq.count == 14
+        print index # Prints answer
+        break
+      end
 
       uniq_chars << char
-      counter += 1
-      uniq_chars = uniq_chars.drop(1) if uniq_chars.length > 14
+      uniq_chars.slice!(0) if uniq_chars.length > 14
     end
   end
-
-  # Print answer:
-  print counter
 end
 
 read('day-6-input.txt')
